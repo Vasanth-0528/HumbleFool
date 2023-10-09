8716464e47a4e594aa91e3570e13fce42d709f3f
Vasanth-0528 <106596896+Vasanth-0528@users.noreply.github.com>
2023-10-09 21:46:01 +0530

    Add files via upload

---
+++ HashMap.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+
+/*
+
+
+a-->2
+k-->1
+h-->1
+i-->1
+
+akhilathop
+
+
+
+unordered_map<int,int>myMap;  
+for(int i=0;i<str.length();i++){
+    if(myMap.find(str[i]) == myMap.end()){
+        myMap[str[i]]=1;   a-->2 k-->1
+    }
+    else{ 
+        myMap[str[i]] = myMap[str[i]]+1; 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+for(int i=0;i<n;i++){
+    cout<<myMap[str[i]];
+}
+
+*/
+
+
+/*
+unordered_map<int,int>myMap;  
+for(int i=0;i<str.length();i++){
+    myMap[str[i]]++; 
+}
+
+
+
+int count[] = new int[26]; -->o(n)
+for(int i=0;i<n;i++){
+    if(mpMap[str[i]]>1){
+        return str[i];
+    }
+}
+
+*/
+
+
+using namespace std;
+int main(){
+    unordered_map<
+    return 0;
+}
